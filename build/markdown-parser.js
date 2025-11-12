86b54ca9d4710b922314577714804f9e060409da
yuekcc <yuekcc@qq.com>
2025-11-13 00:52:40 +0800

    升级依赖，迁移到 markdown-exit

--- build/markdown-parser.js
+++ build/markdown-parser.js
@@ -1,28 +1,33 @@
-import Shiki from '@shikijs/markdown-it';
-import MarkdownIt from 'markdown-it';
+import { codeToHtml, bundledLanguages } from 'shiki';
+import { createMarkdownExit } from 'markdown-exit';
 import taskLists from 'markdown-it-task-lists';
 
-const md = MarkdownIt({ html: true, linkify: true });
-md.use(
-  await Shiki({
-    theme: 'github-dark',
-    fallbackLanguage: 'log',
-  }),
-);
+function findCodeLanguage(lang) {
+  return !!bundledLanguages[lang] ? lang : 'log';
+}
+
+const md = createMarkdownExit({
+  html: true,
+  linkify: true,
+  async highlight(code, lang) {
+    return await codeToHtml(code, { lang: findCodeLanguage(lang?.toLowerCase()), theme: 'github-dark' });
+  },
+});
 md.use(taskLists);
 
 const defaultImageRenderer = md.renderer.rules.image;
-md.renderer.rules.image = function (tokens, idx, options, env, self) {
-  const src = tokens[idx].attrGet('src');
-  console.log('IMG URL', src);
+md.renderer.rules.image = (tokens, idx, options, env, self) => {
+  let src = tokens[idx].attrGet('src');
+  src = src.startsWith('/') ? src.replace(/^\//, '') : src;
 
+  console.log('IMG URL', src);
   tokens[idx].attrSet('src', `/${src}`);
   return defaultImageRenderer(tokens, idx, options, env, self);
 };
 
-md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
   const href = tokens[idx].attrGet('href');
-  console.log('link_open', href)
+  console.log('link_open', href);
   if (/docs\/.+\.md/.test(href)) {
     tokens[idx].attrSet('href', href.replace('docs/', '_docs/'));
   }
@@ -31,5 +36,5 @@ md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
 };
 
 export async function parseMarkdown(filePath, source) {
-  return md.render(source, { filePath });
+  return md.renderAsync(source, { filePath });
 }
